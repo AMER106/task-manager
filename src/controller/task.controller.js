c2a8fcf7ab7a4dd3713366a25948268c01dd977f
AMER106 <arabamer031@gmail.com>
2026-01-22 12:34:19 +0530

    endpoint for delete the task

--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -91,3 +91,21 @@ export const updateTaskController = async (req, res) => {
     });
   }
 };
+
+export const deleteTaskController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndDelete(id);
+    if (!task) {
+      return res.status(404).json({
+        success: false,
+        message: "Task not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Task deleted successfully",
+      data: task,
+    });
+  } catch (error) {}
+};
--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -4,9 +4,11 @@ import {
   createTaskController,
   getMyTasks,
   updateTaskController,
+  deleteTaskController,
 } from "../controller/task.controller.js";
 export const taskRouter = express.Router();
 
 taskRouter.post("/create", auth, createTaskController);
 taskRouter.get("/my-tasks", auth, getMyTasks);
 taskRouter.patch("/update/:id", auth, updateTaskController);
+taskRouter.delete("/delete/:id", auth, deleteTaskController);
